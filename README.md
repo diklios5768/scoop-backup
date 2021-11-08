1e342fcb14cbff69be82fec512ca222894f6e063
diklios <47837462+diklios5768@users.noreply.github.com>
2021-11-08 19:02:02 +0800

    添加备份脚本

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # scoop-backup-pwsh
-scoop backup PowerShell script
+* scoop backup PowerShell script
+
+### 简介
+
+* 因为scoop 至今没有一个很好的备份方法，所以把现有的方法分享出来，虽然也没有达到完美的效果
+
+* 到目前为止，一共只有两个备份脚本
+  * [KNOXDEV/scoop-backup](https://git.irs.sh/KNOXDEV/scoop-backup)
+    * 此脚本在使用Ash258的scoop-core之后报错
+    * 本脚本基于此脚本修改了错误内容
+  * [Cologler](https://github.com/Cologler)/**[ScoopBackup-pwsh](https://github.com/Cologler/ScoopBackup-pwsh)**
+    * 此脚本虽然能够正常运行，但是根据测试，目前只能备份bucket，不能备份app了
+    * 这个脚本的好处是在任何地方都能运行，但是因为对powershell不熟悉，不大会做修改
+
+### 使用
+
+* 先安装错误的scoop-backup
+  * `scoop bucket add knox-scoop https://git.irs.sh/KNOXDEV/knox-scoop`
+  * `scoop install scoop-backup`
+* 进入`apps/scoop-backup`文件夹中替换`scoop-backup.ps1`脚本
+* 备份：`scoop-backup`
+* 导入：在powershell中直接执行备份的脚本
+
---
+++ scoop-backup.ps1
@@ -0,0 +1,115 @@
+#requires -Version 3
+# include functions and argument processing
+$global:arguments = $args
+. "$psscriptroot\functions.ps1"
+
+$supported_arguments = @(
+    (argument 'prints this help message' '-h' '--help' '/?' '-?'),
+    (argument 'compresses the restoration script as an encoded batch file' '-c' '--compress')
+)
+
+# parse various default path settings
+$compressed = passed($supported_arguments[1])
+$default_filename = "backup-$(Get-Date -f yyMMdd)"
+$default_file = "$default_filename" + $(if($compressed) {".bat"} else {".ps1"})
+$default_destination = "$psscriptroot\backups\$default_file"
+$destination = $default_destination
+
+# check for help arguments
+if(passed($supported_arguments[0])) {
+    Write-Host "Usage: scoop-backup [flags] [destination_folder] `n"
+    Write-Host "Default destination: $default_destination `n"
+
+    $supported_arguments | ForEach-Object {
+        Write-Host "$($_.Aliases)   `t $($_.Description)"
+    }
+    break
+}
+
+# filter all paths from our arguments and set our output folder the last path found
+$global:arguments = $arguments | Where {
+    if(Test-Path -Path $_ -PathType container) {
+        $destination = "$_\$default_file"
+        return $false
+    }
+    complain "the following path does not exist or is not a directory: $_"
+    return $true
+}
+
+# complain about unrecognized arguments and abort if found
+if($arguments.Count -ne 0) {
+    complain "unrecognized arguments: $arguments"
+    complain "see: 'scoop-backup --help'"
+    break
+}
+
+
+
+# import core libraries
+try {
+    $scooplib = Resolve-Path "$($(Get-Command scoop).Source)\..\..\apps\scoop\current\lib"
+    . "$scooplib\core.ps1"
+    . "$scooplib\manifest.ps1"
+    . "$scooplib\buckets.ps1"
+    . "$scooplib\versions.ps1"
+} catch {
+    Write-Output "Failed to import Scoop libraries, not found on path"
+    break
+}
+
+# creates initial restoration script content
+$global:cmd = "try{if(Get-Command scoop){}} catch {iex (new-object net.webclient).downloadstring('https://get.scoop.sh')}`n"
+
+# if we need to install some buckets, we'll need to install git first
+$buckets = invoke "Get-LocalBucket" "buckets"
+if(($buckets | Measure-Object).Count -gt 0) {
+    append "scoop install git"
+
+    # add each bucket installation on its own line
+    $buckets | ForEach-Object {
+        # if((known_buckets).Contains($_)) {
+        #     $_
+        # } else {
+            $repo_url = git config --file "$bucketsdir\$_\.git\config" remote.origin.url
+            "$_ $repo_url"
+        # }
+    } | ForEach-Object { append "scoop bucket add $_" }
+}
+
+# next, we install apps
+$apps = installed_apps
+if(($apps | Measure-Object).Count -gt 0) {
+
+    # installing each app on a new line is, unfortunately, more resilient
+    $apps | ForEach-Object {
+        $info = install_info $_ (Select-CurrentVersion $_ $false) $false
+        if($info.url) { $info.url } else { "$($info.bucket)/$_" }
+    } | ForEach-Object { append "scoop install $_" }
+}
+
+# finally, we install global apps
+$globals = installed_apps $true
+if(($globals | Measure-Object).Count -gt 0) {
+    append 'scoop install main/sudo'
+
+    # installing each app on a new line is, unfortunately, more resilient
+    append ('sudo powershell -Command "scoop install --global ' + (($globals | ForEach-Object {
+        $info = install_info $_ (Select-CurrentVersion $_ $true) $true
+        if($info.url) { $info.url } else { "$($info.bucket)/$_" }
+    }) -Join ";scoop install --global ") + '"')
+}
+
+# writing the final output
+New-Item $destination -Force | Out-Null
+if($compressed) {
+    $cmd_bytes = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+    $cmd_encoded = '@echo off' + [environment]::NewLine `
+                + "powershell.exe -NoProfile -EncodedCommand " `
+                + [Convert]::ToBase64String($cmd_bytes) + [environment]::NewLine `
+                + 'pause'
+    Add-Content -Path $destination -Value $cmd_encoded
+} else {
+    Add-Content -Path $destination -Value $cmd
+}
+
+Write-Output "backed up to: $destination"

9a2601ee071fbe6d19777acba1ba08bd193662f1
diklios <47837462+diklios5768@users.noreply.github.com>
2022-03-14 19:43:04 +0800

    add scoop-backup-knox.ps1

--- README.md
+++ README.md
@@ -1,30 +1,36 @@
 # scoop-backup
-* scoop backup **PowerShell** script
 
-### 注意
-* [KNOXDEV/scoop-backup](https://git.irs.sh/KNOXDEV/scoop-backup)问题最近基本修复完毕，即使是使用Ash258的scoop-core也可以备份
+- scoop backup **PowerShell** script
 
-### 简介
+## 原简介
 
-* 因为scoop 至今没有一个很好的备份方法，所以把现有的方法分享出来
+- 因为 scoop 至今没有一个很好的备份方法，所以把现有的方法分享出来
 
-* 到目前为止，一共只有两个备份脚本
-  * [KNOXDEV/scoop-backup](https://git.irs.sh/KNOXDEV/scoop-backup)
-    * 此脚本在使用Ash258的scoop-core之后报错
-      * 但是即使是原生的scoop内核，也会一直有警告
-    * 本脚本基于此脚本修改了错误内容
-  * [Cologler](https://github.com/Cologler)/**[ScoopBackup-pwsh](https://github.com/Cologler/ScoopBackup-pwsh)**
-    * 此脚本虽然能够正常运行，但是根据测试，目前只能备份bucket，不能备份app了
-    * 这个脚本的好处是在任何地方都能运行，但是因为对powershell不熟悉，不大会做修改
+- 到目前为止，一共只有两个备份脚本
+  - [KNOXDEV/scoop-backup](https://git.irs.sh/KNOXDEV/scoop-backup)
+    - 此脚本在使用 Ash258 的 scoop-core 之后报错
+      - 但是即使是原生的 scoop 内核，也会一直有黄色的警告
+    - 本脚本基于此脚本修改了错误内容
+      - 2022.1.5 此脚本基本修复完毕，可以正常使用
+  - [Cologler](https://github.com/Cologler)/**[ScoopBackup-pwsh](https://github.com/Cologler/ScoopBackup-pwsh)**
+    - 此脚本虽然能够正常运行，但是根据测试，目前只能备份 bucket，不能备份 app 了
+    - 这个脚本的好处是在任何地方都能运行，但是因为对 powershell 不熟悉，不大会做修改
 
 ### 使用
 
-* 安装我的bucket：`scoop bucket add diklios https://github.com/diklios5768/diklios-scoop-bucket`
-* 安装备份脚本：`scoop install diklios/scoop-backup`
-* 备份：`scoop-backup`
-  * 备份完会提示备份文件位置
-* 重新安装：直接执行PowerShell脚本即可
+- 安装我的 bucket：`scoop bucket add diklios https://github.com/diklios5768/diklios-scoop-bucket`
+- 安装备份脚本：`scoop install diklios/scoop-backup`
+- 备份：`scoop-backup`
+  - 备份完会提示备份文件位置
+- 重新安装：直接执行 PowerShell 脚本即可
 
 ### 注意
-* 如果安装了main bucket中的 scoop-search，请注意千万别加hooks，因为[#13](https://github.com/shilangyu/scoop-search/issues/13)，所以不仅命令行中的help会报错，这个备份脚本也会出错
-  * 建议用别名，或者直接手输`scoop-search`，这问题暂时不好解决
+
+- 如果安装了 main bucket 中的 scoop-search，请注意千万别加 hooks，因为[#13](https://github.com/shilangyu/scoop-search/issues/13)，所以不仅命令行中的 help 会报错，这个备份脚本也会出错
+  - 建议用别名，或者直接手输`scoop-search`，这问题暂时不好解决
+- [KNOXDEV/scoop-backup](https://git.irs.sh/KNOXDEV/scoop-backup)问题基本修复完毕，即使是使用 Ash258 的 scoop-core 也可以备份
+  - 但是由于一些原因，基本无法安装成功，所以推荐使用我的备份脚本
+  - 安装我的 bucket：`scoop bucket add diklios https://github.com/diklios5768/diklios-scoop-bucket`
+  - 安装备份脚本：`scoop install diklios/scoop-backup-knox`
+  - 使用方法和之前一样，请不要把两个脚本同时安装上，只会生效后一个安装的脚本
+    - 因为没必要使用两种备份脚本，所以为命令懒得改名了
---
+++ scoop-backup-knox.ps1
@@ -0,0 +1,127 @@
+#requires -Version 3
+# include functions and argument processing
+$global:arguments = $args
+. "$psscriptroot\functions.ps1"
+
+$supported_arguments = @(
+    (argument 'prints this help message' '-h' '--help' '/?' '-?'),
+    (argument 'compresses the restoration script as an encoded batch file' '-c' '--compress')
+)
+
+# parse various default path settings
+$compressed = passed($supported_arguments[1])
+$default_filename = "backup-$(Get-Date -f yyMMdd)"
+$default_file = "$default_filename" + $(if($compressed) {".bat"} else {".ps1"})
+$default_destination = "$psscriptroot\backups\$default_file"
+$destination = $default_destination
+
+# check for help arguments
+if(passed($supported_arguments[0])) {
+    Write-Host "Usage: scoop-backup [flags] [destination_folder] `n"
+    Write-Host "Default destination: $default_destination `n"
+
+    $supported_arguments | ForEach-Object {
+        Write-Host "$($_.Aliases)   `t $($_.Description)"
+    }
+    break
+}
+
+# filter all paths from our arguments and set our output folder the last path found
+$global:arguments = $arguments | Where {
+    if(Test-Path -Path $_ -PathType container) {
+        $destination = "$_\$default_file"
+        return $false
+    }
+    complain "the following path does not exist or is not a directory: $_"
+    return $true
+}
+
+# complain about unrecognized arguments and abort if found
+if($arguments.Count -ne 0) {
+    complain "unrecognized arguments: $arguments"
+    complain "see: 'scoop-backup --help'"
+    break
+}
+
+
+
+# import core libraries
+try {
+    $scooplib = Resolve-Path "$($(Get-Command scoop).Source)\..\..\apps\scoop\current\lib"
+    . "$scooplib\core.ps1"
+    . "$scooplib\commands.ps1"
+    . "$scooplib\help.ps1"
+    . "$scooplib\manifest.ps1"
+    . "$scooplib\buckets.ps1"
+    . "$scooplib\versions.ps1"
+} catch {
+    Write-Output "Failed to import Scoop libraries, not found on path"
+    break
+}
+
+# creates initial restoration script content
+$global:cmd = "if (Get-Command -Name scoop -ErrorAction SilentlyContinue) {} else {iwr -useb get.scoop.sh | iex}`n"
+
+# if we need to install some buckets, we'll need to install git first
+$buckets = invoke "Get-LocalBucket" "buckets"
+if(($buckets | Measure-Object).Count -gt 0) {
+    append "scoop install git"
+
+    # add each bucket installation on its own line
+    $buckets | ForEach-Object {
+        $repo_url = git config --file "$bucketsdir\$_\.git\config" remote.origin.url
+        "$_ $repo_url"
+    } | ForEach-Object { append "scoop bucket add $_" }
+}
+
+# next, we install apps
+$apps = installed_apps
+if(($apps | Measure-Object).Count -gt 0) {
+
+    # installing each app on a new line is, unfortunately, more resilient
+    $apps | ForEach-Object {
+        $info = install_info $_ (Select-CurrentVersion -AppName $_ -Global:$false) $false
+        if($info.url) { $info.url } else { "$($info.bucket)/$_" }
+    } | ForEach-Object { append "scoop install $_" }
+}
+
+# next, we install global apps
+$globals = installed_apps $true
+if(($globals | Measure-Object).Count -gt 0) {
+    append 'scoop install main/sudo'
+
+    # installing each app on a new line is, unfortunately, more resilient
+    append ('sudo powershell -Command "scoop install --global ' + (($globals | ForEach-Object {
+        $info = install_info $_ (Select-CurrentVersion -AppName $_ -Global:$true) $true
+        if($info.url) { $($info.url) } else { "$($info.bucket)/$_" }
+    }) -Join ";scoop install --global ") + '"')
+}
+
+# finally, we install any scoop aliases
+# I've noticed that on old configs from scoop some of this operations will error, hense the try
+try {
+    (get_config 'alias').PSObject.Properties.GetEnumerator() | ForEach-Object {
+        $content = Get-Content (command_path $_.Name)
+        $command = ($content | Select-Object -Skip 1).Trim()
+        $summary = (summary $content).Trim()
+
+        append "scoop alias add '$($_.name)' '$command' '$summary'"
+    }
+} catch {
+    Write-Output "Failed to enumerate aliases, not backed up"
+}
+
+# writing the final output
+New-Item $destination -Force | Out-Null
+if($compressed) {
+    $cmd_bytes = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+    $cmd_encoded = '@echo off' + [environment]::NewLine `
+                + "powershell.exe -NoProfile -EncodedCommand " `
+                + [Convert]::ToBase64String($cmd_bytes) + [environment]::NewLine `
+                + 'pause'
+    Add-Content -Path $destination -Value $cmd_encoded
+} else {
+    Add-Content -Path $destination -Value $cmd
+}
+
+Write-Output "backed up to: $destination"
